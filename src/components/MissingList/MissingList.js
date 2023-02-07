446b92d2048b271cacb3928930e67d2bb5abc307
onuruci <onur.yuce@metu.edu.tr>
2023-02-07 23:18:41 +0300

    Update MissingList.js

--- src/components/MissingList/MissingList.js
+++ src/components/MissingList/MissingList.js
@@ -2,11 +2,31 @@ import React from "react";
 
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
+import MenuItem from "@mui/material/MenuItem";
 
 import { MISSING_LIST } from "../../utils/data";
 import { ChildCard } from "../ChildCard/ChildCard";
 
 export const MissingList = () => {
+  const cities = [
+    {
+      value: "Hatay",
+      label: "Hatay",
+    },
+    {
+      value: "Maraş",
+      label: "Maraş",
+    },
+    {
+      value: "Diyarbakır",
+      label: "Diyarbakır",
+    },
+    {
+      value: "Osmaniye",
+      label: "Osmaniye",
+    },
+  ];
+
   return (
     <div>
       <Box
@@ -21,44 +41,31 @@ export const MissingList = () => {
           <TextField
             required
             id="outlined-required"
-            label="Required"
-            defaultValue="Hello World"
-          />
-          <TextField
-            disabled
-            id="outlined-disabled"
-            label="Disabled"
-            defaultValue="Hello World"
-          />
-          <TextField
-            id="outlined-password-input"
-            label="Password"
-            type="password"
-            autoComplete="current-password"
-          />
-          <TextField
-            id="outlined-read-only-input"
-            label="Read Only"
-            defaultValue="Hello World"
-            InputProps={{
-              readOnly: true,
-            }}
+            label="İsim Soyisim"
+            defaultValue=""
           />
           <TextField
             id="outlined-number"
-            label="Number"
+            label="Yaş"
+            required
             type="number"
             InputLabelProps={{
               shrink: true,
             }}
           />
-          <TextField id="outlined-search" label="Search field" type="search" />
           <TextField
-            id="outlined-helperText"
-            label="Helper text"
-            defaultValue="Default Value"
-            helperText="Some important text"
-          />
+            id="outlined-select-currency"
+            select
+            label="Şehir"
+            defaultValue="EUR"
+            helperText="Çocuğun kaybolduğu şehir"
+          >
+            {cities.map((option) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.label}
+              </MenuItem>
+            ))}
+          </TextField>
         </div>
       </Box>
       <div className="mt-8 grid gap-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
